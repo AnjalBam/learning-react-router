d64f8bc11f6dbb419b63c0e8b8baecf70f4c5993
Anjal Bam <anjalbam81@gmail.com>
2020-05-16 21:59:06 +0545

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <h1>React Routing Example</h1>
+      <IndexPage />
+      <AboutPage />
+    </section>
   );
 }
+
+const IndexPage = () => {
+  return <h3>Home Page</h3>;
+};
+
+const AboutPage = () => {
+  return <h3>About Page</h3>;
+};
 
 export default App;
